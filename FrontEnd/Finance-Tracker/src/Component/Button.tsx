df1137f6facee9229f88b407388615da4a65719d
shrawan <mundushrm@gmail.com>
2025-11-17 14:45:13 +0545

    frontend login setup test

---
+++ FrontEnd/Finance-Tracker/src/Component/Button.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  variant?: "primary" | "outline";
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
+
+export default function Buttonn({
+  children,
+  variant = "primary",
+  onClick,
+  type,
+}: ButtonProps) {
+  const base = "px-4 py-2 rounded-md font-small transition";
+  const styles =
+    variant === "primary"
+      ? "bg-[#299D91] hover:bg-blue-600 text-white hover:cursor-pointer w-full hover:scale-103"
+      : "bg-red-500 hover:bg-blue-600 text-white  hover:cursor-pointer w-full hover:scale-103";
+  return (
+    <button className={`${base} ${styles}`} onClick={onClick} type={type}>
+      {children}
+    </button>
+  );
+}
---
+++ FrontEnd/Finance-Tracker/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login.tsx";
+
+const route = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={route} />
+  </StrictMode>
+);
